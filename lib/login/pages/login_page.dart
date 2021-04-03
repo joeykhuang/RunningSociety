7a4cbdbbe1119f3c632b2ff1f7ab727d69e140d5
josephhuxley <joeykhuang@gmail.com>
2021-04-03 23:34:05 +0800

    added login/signup

--- lib/login/pages/login_page.dart
+++ lib/login/pages/login_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:running_society/theme.dart';
+import 'package:running_society/login/widgets/snackbar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import 'widgets/sign_in.dart';
-import 'widgets/sign_up.dart';
+import 'sign_up.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -15,12 +16,22 @@ class _LoginPageState extends State<LoginPage>
     with SingleTickerProviderStateMixin {
   late PageController _pageController;
 
-  Color left = Colors.black;
-  Color right = Colors.white;
+  Color left = Colors.deepOrangeAccent;
+  Color right = Colors.black;
+
+  TextEditingController loginEmailController = TextEditingController();
+  TextEditingController loginPasswordController = TextEditingController();
+
+  final FocusNode focusNodeEmail = FocusNode();
+  final FocusNode focusNodePassword = FocusNode();
+
+  bool _obscureTextPassword = true;
 
   @override
   void dispose() {
-    _pageController?.dispose();
+    focusNodeEmail.dispose();
+    focusNodePassword.dispose();
+    _pageController.dispose();
     super.dispose();
   }
 
@@ -64,40 +75,169 @@ class _LoginPageState extends State<LoginPage>
                     fit: BoxFit.fill,
                     image: const AssetImage('assets/img/login_logo.png')),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: _buildMenuBar(context),
+              Stack(
+                alignment: Alignment.topCenter,
+                children: <Widget>[
+                  Card(
+                    elevation: 2.0,
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Container(
+                      width: 300.0,
+                      height: 190.0,
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 20.0, bottom: 20.0, left: 25.0, right: 25.0),
+                            child: TextField(
+                              focusNode: focusNodeEmail,
+                              controller: loginEmailController,
+                              keyboardType: TextInputType.emailAddress,
+                              style: const TextStyle(
+                                  fontFamily: 'WorkSansSemiBold',
+                                  fontSize: 16.0,
+                                  color: Colors.black),
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                icon: Icon(
+                                  FontAwesomeIcons.envelope,
+                                  color: Colors.black,
+                                  size: 22.0,
+                                ),
+                                hintText: 'Email Address',
+                                hintStyle: TextStyle(
+                                    fontFamily: 'WorkSansSemiBold', fontSize: 17.0),
+                              ),
+                              onSubmitted: (_) {
+                                focusNodePassword.requestFocus();
+                              },
+                            ),
+                          ),
+                          Container(
+                            width: 250.0,
+                            height: 1.0,
+                            color: Colors.grey[400],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 20.0, bottom: 20.0, left: 25.0, right: 25.0),
+                            child: TextField(
+                              focusNode: focusNodePassword,
+                              controller: loginPasswordController,
+                              obscureText: _obscureTextPassword,
+                              style: const TextStyle(
+                                  fontFamily: 'WorkSansSemiBold',
+                                  fontSize: 16.0,
+                                  color: Colors.black),
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                icon: const Icon(
+                                  FontAwesomeIcons.lock,
+                                  size: 22.0,
+                                  color: Colors.black,
+                                ),
+                                hintText: 'Password',
+                                hintStyle: const TextStyle(
+                                    fontFamily: 'WorkSansSemiBold', fontSize: 17.0),
+                                suffixIcon: GestureDetector(
+                                  onTap: _toggleLogin,
+                                  child: Icon(
+                                    _obscureTextPassword
+                                        ? FontAwesomeIcons.eye
+                                        : FontAwesomeIcons.eyeSlash,
+                                    size: 15.0,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              onSubmitted: (_) {
+                                _toggleSignInButton();
+                              },
+                              textInputAction: TextInputAction.go,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 170.0),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                          color: CustomTheme.loginGradientStart,
+                          offset: Offset(1.0, 6.0),
+                          blurRadius: 20.0,
+                        ),
+                        BoxShadow(
+                          color: CustomTheme.loginGradientEnd,
+                          offset: Offset(1.0, 6.0),
+                          blurRadius: 20.0,
+                        ),
+                      ],
+                      gradient: LinearGradient(
+                          colors: <Color>[
+                            Colors.deepOrangeAccent,
+                            Colors.orangeAccent,
+                          ],
+                          begin: FractionalOffset(0.2, 0.2),
+                          end: FractionalOffset(1.0, 1.0),
+                          stops: <double>[0.0, 1.0],
+                          tileMode: TileMode.clamp),
+                    ),
+                    child: MaterialButton(
+                      highlightColor: Colors.transparent,
+                      splashColor: Colors.deepOrangeAccent,
+                      child: const Padding(
+                        padding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 42.0),
+                        child: Text(
+                          'LOGIN',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25.0,
+                              fontFamily: 'WorkSansBold'),
+                        ),
+                      ),
+                      onPressed: () => CustomSnackBar(
+                          context, const Text('Login button pressed')),
+                    ),
+                  )
+                ],
               ),
-              Expanded(
-                flex: 2,
-                child: PageView(
-                  controller: _pageController,
-                  physics: const ClampingScrollPhysics(),
-                  onPageChanged: (int i) {
-                    FocusScope.of(context).requestFocus(FocusNode());
-                    if (i == 0) {
-                      setState(() {
-                        right = Colors.white;
-                        left = Colors.black;
-                      });
-                    } else if (i == 1) {
-                      setState(() {
-                        right = Colors.black;
-                        left = Colors.white;
-                      });
-                    }
-                  },
-                  children: <Widget>[
-                    ConstrainedBox(
-                      constraints: const BoxConstraints.expand(),
-                      child: const SignIn(),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Forgot Password?',
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.black,
+                          fontSize: 16.0,
+                          fontFamily: 'WorkSansMedium'),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: TextButton(
+                    onPressed: () => Navigator.of(context).push<void>(
+                      MaterialPageRoute(
+                        builder: (context) => SignUp(),
+                      ),
                     ),
-                    ConstrainedBox(
-                      constraints: const BoxConstraints.expand(),
-                      child: const SignUp(),
-                    ),
-                  ],
-                ),
+                    child: const Text(
+                      'Sign Up?',
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.black,
+                          fontSize: 16.0,
+                          fontFamily: 'WorkSansMedium'),
+                    )),
               ),
             ],
           ),
@@ -106,60 +246,14 @@ class _LoginPageState extends State<LoginPage>
     ));
   }
 
-  Widget _buildMenuBar(BuildContext context) {
-    return Container(
-      width: 300.0,
-      height: 50.0,
-      decoration: const BoxDecoration(
-        color: Color(0x552B2B2B),
-        borderRadius: BorderRadius.all(Radius.circular(25.0)),
-      ),
-      child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Expanded(
-              child: TextButton(
-                style: ButtonStyle(
-                  overlayColor: MaterialStateProperty.all(Colors.transparent),
-                ),
-                onPressed: _onSignInButtonPress,
-                child: Text(
-                  'Existing',
-                  style: TextStyle(
-                      color: left,
-                      fontSize: 16.0,
-                      fontFamily: 'WorkSansSemiBold'),
-                ),
-              ),
-            ),
-            //Container(height: 33.0, width: 1.0, color: Colors.white),
-            Expanded(
-              child: TextButton(
-                style: ButtonStyle(
-                  overlayColor: MaterialStateProperty.all(Colors.transparent),
-                ),
-                onPressed: _onSignUpButtonPress,
-                child: Text(
-                  'New',
-                  style: TextStyle(
-                      color: right,
-                      fontSize: 16.0,
-                      fontFamily: 'WorkSansSemiBold'),
-                ),
-              ),
-            ),
-          ],
-        ),
-    );
+  void _toggleSignInButton() {
+    CustomSnackBar(context, const Text('Login button pressed'));
   }
 
-  void _onSignInButtonPress() {
-    _pageController.animateToPage(0,
-        duration: const Duration(milliseconds: 500), curve: Curves.decelerate);
+  void _toggleLogin() {
+    setState(() {
+      _obscureTextPassword = !_obscureTextPassword;
+    });
   }
 
-  void _onSignUpButtonPress() {
-    _pageController?.animateToPage(1,
-        duration: const Duration(milliseconds: 500), curve: Curves.decelerate);
-  }
 }
---
+++ lib/login/pages/sign_up.dart
@@ -0,0 +1,321 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:running_society/login/widgets/snackbar.dart';
+import 'package:running_society/theme.dart';
+import 'login_page.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final FocusNode focusNodePassword = FocusNode();
+  final FocusNode focusNodeConfirmPassword = FocusNode();
+  final FocusNode focusNodeEmail = FocusNode();
+  final FocusNode focusNodeName = FocusNode();
+
+  bool _obscureTextPassword = true;
+  bool _obscureTextConfirmPassword = true;
+
+  TextEditingController signupEmailController = TextEditingController();
+  TextEditingController signupNameController = TextEditingController();
+  TextEditingController signupPasswordController = TextEditingController();
+  TextEditingController signupConfirmPasswordController =
+      TextEditingController();
+
+  @override
+  void dispose() {
+    focusNodePassword.dispose();
+    focusNodeConfirmPassword.dispose();
+    focusNodeEmail.dispose();
+    focusNodeName.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+          physics: const ClampingScrollPhysics(),
+          child: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).requestFocus(FocusNode());
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    colors: <Color>[
+                      CustomTheme.loginGradientStart,
+                      CustomTheme.loginGradientEnd
+                    ],
+                    begin: FractionalOffset(0.0, 0.0),
+                    end: FractionalOffset(1.0, 1.0),
+                    stops: <double>[0.0, 1.0],
+                    tileMode: TileMode.clamp),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 75.0),
+                    child: Image(
+                        height:
+                        MediaQuery.of(context).size.height > 800 ? 191.0 : 150,
+                        fit: BoxFit.fill,
+                        image: const AssetImage('assets/img/login_logo.png')),
+                  ),
+                  Stack(
+                    alignment: Alignment.topCenter,
+                    children: <Widget>[
+                      Card(
+                        elevation: 2.0,
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: Container(
+                          width: 300.0,
+                          height: 360.0,
+                          child: Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 20.0, bottom: 5, left: 25.0, right: 25.0),
+                                child: TextField(
+                                  focusNode: focusNodeName,
+                                  controller: signupNameController,
+                                  keyboardType: TextInputType.text,
+                                  textCapitalization: TextCapitalization.words,
+                                  autocorrect: false,
+                                  style: const TextStyle(
+                                      fontFamily: 'WorkSansSemiBold',
+                                      fontSize: 16.0,
+                                      color: Colors.black),
+                                  decoration: const InputDecoration(
+                                    border: InputBorder.none,
+                                    icon: Icon(
+                                      FontAwesomeIcons.user,
+                                      color: Colors.black,
+                                    ),
+                                    hintText: 'Name',
+                                    hintStyle: TextStyle(
+                                        fontFamily: 'WorkSansSemiBold', fontSize: 16.0),
+                                  ),
+                                  onSubmitted: (_) {
+                                    focusNodeEmail.requestFocus();
+                                  },
+                                ),
+                              ),
+                              Container(
+                                width: 250.0,
+                                height: 1.0,
+                                color: Colors.grey[400],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 20.0, bottom: 5, left: 25.0, right: 25.0),
+                                child: TextField(
+                                  focusNode: focusNodeEmail,
+                                  controller: signupEmailController,
+                                  keyboardType: TextInputType.emailAddress,
+                                  autocorrect: false,
+                                  style: const TextStyle(
+                                      fontFamily: 'WorkSansSemiBold',
+                                      fontSize: 16.0,
+                                      color: Colors.black),
+                                  decoration: const InputDecoration(
+                                    border: InputBorder.none,
+                                    icon: Icon(
+                                      FontAwesomeIcons.envelope,
+                                      color: Colors.black,
+                                    ),
+                                    hintText: 'Email Address',
+                                    hintStyle: TextStyle(
+                                        fontFamily: 'WorkSansSemiBold', fontSize: 16.0),
+                                  ),
+                                  onSubmitted: (_) {
+                                    focusNodePassword.requestFocus();
+                                  },
+                                ),
+                              ),
+                              Container(
+                                width: 250.0,
+                                height: 1.0,
+                                color: Colors.grey[400],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 20.0, bottom: 5, left: 25.0, right: 25.0),
+                                child: TextField(
+                                  focusNode: focusNodePassword,
+                                  controller: signupPasswordController,
+                                  obscureText: _obscureTextPassword,
+                                  autocorrect: false,
+                                  style: const TextStyle(
+                                      fontFamily: 'WorkSansSemiBold',
+                                      fontSize: 16.0,
+                                      color: Colors.black),
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    icon: const Icon(
+                                      FontAwesomeIcons.lock,
+                                      color: Colors.black,
+                                    ),
+                                    hintText: 'Password',
+                                    hintStyle: const TextStyle(
+                                        fontFamily: 'WorkSansSemiBold', fontSize: 16.0),
+                                    suffixIcon: GestureDetector(
+                                      onTap: _toggleSignup,
+                                      child: Icon(
+                                        _obscureTextPassword
+                                            ? FontAwesomeIcons.eye
+                                            : FontAwesomeIcons.eyeSlash,
+                                        size: 15.0,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                  onSubmitted: (_) {
+                                    focusNodeConfirmPassword.requestFocus();
+                                  },
+                                ),
+                              ),
+                              Container(
+                                width: 250.0,
+                                height: 1.0,
+                                color: Colors.grey[400],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 20.0, bottom: 5.0, left: 25.0, right: 25.0),
+                                child: TextField(
+                                  focusNode: focusNodeConfirmPassword,
+                                  controller: signupConfirmPasswordController,
+                                  obscureText: _obscureTextConfirmPassword,
+                                  autocorrect: false,
+                                  style: const TextStyle(
+                                      fontFamily: 'WorkSansSemiBold',
+                                      fontSize: 16.0,
+                                      color: Colors.black),
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    icon: const Icon(
+                                      FontAwesomeIcons.lock,
+                                      color: Colors.black,
+                                    ),
+                                    hintText: 'Confirm Password',
+                                    hintStyle: const TextStyle(
+                                        fontFamily: 'WorkSansSemiBold', fontSize: 16.0),
+                                    suffixIcon: GestureDetector(
+                                      onTap: _toggleSignupConfirm,
+                                      child: Icon(
+                                        _obscureTextConfirmPassword
+                                            ? FontAwesomeIcons.eye
+                                            : FontAwesomeIcons.eyeSlash,
+                                        size: 15.0,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                  onSubmitted: (_) {
+                                    _toggleSignUpButton();
+                                  },
+                                  textInputAction: TextInputAction.go,
+                                ),
+                              ),
+                              Container(
+                                width: 250.0,
+                                height: 1.0,
+                                color: Colors.grey[400],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 340.0),
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              color: CustomTheme.loginGradientStart,
+                              offset: Offset(1.0, 6.0),
+                              blurRadius: 20.0,
+                            ),
+                            BoxShadow(
+                              color: CustomTheme.loginGradientEnd,
+                              offset: Offset(1.0, 6.0),
+                              blurRadius: 20.0,
+                            ),
+                          ],
+                          gradient: LinearGradient(
+                              colors: <Color>[
+                                Colors.deepOrangeAccent,
+                                Colors.orangeAccent,
+                              ],
+                              begin: FractionalOffset(0.2, 0.2),
+                              end: FractionalOffset(1.0, 1.0),
+                              stops: <double>[0.0, 1.0],
+                              tileMode: TileMode.clamp),
+                        ),
+                        child: MaterialButton(
+                          highlightColor: Colors.transparent,
+                          splashColor: Colors.deepOrangeAccent,
+                          //shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5.0))),
+                          child: const Padding(
+                            padding:
+                            EdgeInsets.symmetric(vertical: 10.0, horizontal: 42.0),
+                            child: Text(
+                              'SIGN UP',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 25.0,
+                                  fontFamily: 'WorkSansBold'),
+                            ),
+                          ),
+                          onPressed: () => _toggleSignUpButton(),
+                        ),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: TextButton(
+                        onPressed: () => Navigator.of(context).pop<void>(),
+                        child: const Text(
+                          'Cancel',
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              color: Colors.black,
+                              fontSize: 16.0,
+                              fontFamily: 'WorkSansMedium'),
+                        )),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+
+  void _toggleSignUpButton() {
+    CustomSnackBar(context, const Text('SignUp button pressed'));
+  }
+
+  void _toggleSignup() {
+    setState(() {
+      _obscureTextPassword = !_obscureTextPassword;
+    });
+  }
+
+  void _toggleSignupConfirm() {
+    setState(() {
+      _obscureTextConfirmPassword = !_obscureTextConfirmPassword;
+    });
+  }
+}
